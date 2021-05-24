8b293c7576956ef5a3509052116438a90e048e4f
rubbalpreetsingh <rubbalpreetsingh03@gmail.com>
2021-05-24 20:06:51 +0530

    Standard input

--- questions.js
+++ questions.js
@@ -6,7 +6,7 @@
 const questions = [
     "What is your Name?",
     "Where are you from?",
-    "What is your prederred programming language?"
+    "What is your preferred programming language?"
 ];
 
 const ask = (i=0)=>{
@@ -16,3 +16,39 @@ const ask = (i=0)=>{
 
 
 ask();
+
+
+const answers = [];
+
+
+process.stdin.on('data',data=>{
+    answers.push(data.toString().trim());
+    if(answers.length < questions.length){
+        ask(answers.length);
+    }else{
+        process.exit();
+    }
+});
+
+process.on("exit",()=>{
+    const [name, from, lang] = answers;
+    console.log(`
+    
+    Thank you for your answers.
+
+    Your Name: ${name},
+    You from: ${from},
+    Preferred Language: ${lang}
+    
+    
+    `);
+});
+
+
+/*
+process.stdin.on('data',data=>{
+    process.stdout.write(`\n\n ${data.toString().trim()}\n`);
+    process.exit();
+});*/
+
+
