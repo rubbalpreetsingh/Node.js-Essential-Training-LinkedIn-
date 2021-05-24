aab9e2f936cf943b0d92308d77d836122f6e1278
rubbalpreetsingh <rubbalpreetsingh03@gmail.com>
2021-05-24 20:23:29 +0530

    Incorporate setInterval

--- timers.js
+++ timers.js
@@ -1,7 +1,25 @@
 const waitTimer = 3000;
+
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = ()=>{
+    currentTime += waitInterval;
+    console.log(`wait ${currentTime/1000} seconds`);
+};
+
+
 console.log(`setting a ${waitTimer/1000} second delay`);
 
-const timeFinished= ()=> console.log("Done");
+const timeFinished= ()=>{
+    clearInterval(interval);
+    console.log("Done");
+};
 
+
+const interval = setInterval(incTime, waitInterval);
 
 setTimeout(timeFinished, waitTimer);
+
+
+
