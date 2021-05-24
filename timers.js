6d19f26c51c8d7648dd083063191ba3bc54f8b1d
rubbalpreetsingh <rubbalpreetsingh03@gmail.com>
2021-05-24 20:36:53 +0530

    process.stdout.clearLine();

--- timers.js
+++ timers.js
@@ -1,11 +1,14 @@
-const waitTimer = 3000;
+const waitTimer = 5000;
 
 const waitInterval = 500;
 let currentTime = 0;
 
 const incTime = ()=>{
     currentTime += waitInterval;
-    console.log(`wait ${currentTime/1000} seconds`);
+    const p = Math.floor((currentTime/waitTimer)*100);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`waiting... ${p} %`);
 };
 
 
@@ -13,6 +16,8 @@ console.log(`setting a ${waitTimer/1000} second delay`);
 
 const timeFinished= ()=>{
     clearInterval(interval);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
     console.log("Done");
 };
 
